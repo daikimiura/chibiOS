27c35cbee6a996bdb9929567d9b2f73830c1e726
daikimiura <daikiii5555@gmail.com>
2020-08-09 22:43:57 +0900

    Add 16.7

---
+++ src/21_draw_color_bar/kernel.s
@@ -0,0 +1,37 @@
+%include        "../include/define.s"
+%include        "../include/macro.s"
+
+        ORG     KERNEL_LOAD
+[BITS 32]
+
+kernel:
+        ; フォントアドレスを取得
+        mov     esi, BOOT_LOAD + SECT_SIZE
+        movzx   eax, word [esi + 0] ; セグメント FONT.sg
+        movzx   ebx, word [esi + 2] ; オフセット FONT.off
+        shl     eax, 4
+        add     eax, ebx ; セグメント:オフセット の形にする
+        mov     [FONT_ADR], eax
+
+        ; フォントの表示
+        cdecl   draw_font, 63, 13
+
+        ; 文字列の表示
+        cdecl   draw_str, 25, 14, 0x010F, .s0
+
+        ; カラーバーの表示
+        cdecl   draw_color_bar, 63, 4
+
+        jmp     $
+.s0     db      "Hello, kernel!", 0
+ALIGN 4, db     0
+FONT_ADR: dd     0
+
+; モジュール
+%include        "../modules/protect/vga.s"
+%include        "../modules/protect/draw_char.s"
+%include        "../modules/protect/draw_font.s"
+%include        "../modules/protect/draw_str.s"
+%include        "../modules/protect/draw_color_bar.s"
+
+        times KERNEL_SIZE - ($ - $$)    db  0
